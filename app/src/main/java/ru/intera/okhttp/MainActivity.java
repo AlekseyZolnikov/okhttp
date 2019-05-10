d38ae8908d97d83955ac64c491cee55878ce35e5
Aleksey Zolnikov <monsarda@yandex.ru>
2019-05-10 14:21:27 +0300

    Call Requester in Main Activity

--- app/src/main/java/ru/intera/okhttp/MainActivity.java
+++ app/src/main/java/ru/intera/okhttp/MainActivity.java
@@ -2,12 +2,40 @@ package ru.intera.okhttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView page;
+    private EditText searchEditText;
+    private Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        page = findViewById(R.id.webview);
+        searchEditText = findViewById(R.id.search_edit_text);
+        searchButton = findViewById(R.id.button);
+
+        final OkHttpRequester requester = new OkHttpRequester(new OkHttpRequester.OnResponseCompleted() {
+            @Override
+            public void onCompleted(String content) {
+                page.loadData(content, "text/html; charset=utf-8", "utf-8");
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = searchEditText.getText().toString();
+                requester.run(query);
+
+            }
+        });
     }
 }
